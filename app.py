4412d40817c25a172e499f4e6e5dfc8001f24f01
Gael AMOUSSOU <gael-ghislain.amoussou@epitech.eu>
2023-01-28 14:32:19 +0100

    creation des tables

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from flask import render_template
+from flask_migrate import Migrate
+from models import *
+
 app = Flask(__name__)
+app.config.from_object('config')
+db.init_app(app)
+migrate = Migrate(app, db)
+# app.template_folder = 'template'
+
+
+@app.route('/')
+def index():
+    return render_template('pages/index.html')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ models.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, TIMESTAMP
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    surname = db.Column(db.String(100))
+    firstname = db.Column(db.String(100))
+    birthdate = db.Column(db.String(30))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(20))
+    gender = db.Column(db.Boolean)
+    avatar = db.Column(db.String(500))
+    email = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+    year = db.relationship('Year', uselist=False, backref='user')
+    role = db.Column(db.Boolean, default=False, nullable=False)
+    account_status = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = Column(TIMESTAMP)
+    updated_at = Column(TIMESTAMP)
+
+
+class Role(db.Model):
+    __tablename__ = 'role'
+
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(80))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('roles', lazy='dynamic'))
+    created_at = Column(TIMESTAMP)
+    updated_at = Column(TIMESTAMP)
+
+
+class year(db.Model):
+    __tablename__ = 'year'
+
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(80))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = Column(TIMESTAMP)
+    updated_at = Column(TIMESTAMP)
